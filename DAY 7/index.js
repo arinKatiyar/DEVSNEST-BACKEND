2a18c4f343e35add6dcaf17c046b6bc9965fa6bf
Arin Katiyar <63258407+arinKatiyar@users.noreply.github.com>
2021-09-21 22:29:48 +0530

    Add files via upload

---
+++ DAY 7/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(5000);
+
+
+app.get("/withjson", (req, res) => {
+    res.json({ a: 1 });
+
+})
+app.get("/status", (req, res) => {
+    res.status(200);   
+    res.send("hemlo");
+})
+
+
+
+app.get("/statusi", (req, res) => {                 
+    res.sendStatus(500).json({ a: 1 });
+})
